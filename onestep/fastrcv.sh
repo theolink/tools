a76a207f532ad3301c5ca548d61039184f231d59
theolink <theowu@qq.com>
2019-04-26 21:52:56 +0800

    update faststr.sh fastrcv.sh

--- onestep/fastrcv.sh
+++ onestep/fastrcv.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
-cd
-git clone https://github.com/theolink/rtklib.git
-cd rtklib/app/rtkrcv/gcc
-make
-sudo cp rtkrcv /usr/local/bin/rtkrcv
-sudo chmod +x /usr/local/bin/rtkrcv
-sudo mkdir /etc/rtk/
-sudo mkdir /etc/rtk/log/
-sudo mkdir /etc/rtk/cmd/
-sudo chmod -R 777 /etc/rtk
+cd /usr/local/bin/
+wget https://github.com/theolink/tools/raw/master/rtk/rtkrcv
+chmod +x /usr/local/bin/rtkrcv
+mkdir /etc/rtk/
+mkdir /etc/rtk/log/
+mkdir /etc/rtk/cmd/
+chmod -R 777 /etc/rtk
 cd /etc/rtk/cmd/
 wget https://raw.githubusercontent.com/theolink/tools/master/rtk/m8t_5hz_uart_beidou.cmd
 cd /etc/rtk/
 wget https://raw.githubusercontent.com/theolink/tools/master/rtk/rtkrcv.conf
 wget https://raw.githubusercontent.com/theolink/tools/master/rtk/rcvinit.sh
-sudo chmod +x /etc/rtk/rcvinit.sh
-sudo touch /etc/rtk/log/rcvinit.log
-sudo chmod 777 /etc/rtk/log/rcvinit.log
+chmod +x /etc/rtk/rcvinit.sh
+touch /etc/rtk/log/rcvinit.log
+chmod 777 /etc/rtk/log/rcvinit.log
 sed -i '16i\/etc/rtk/rcvinit.sh\n' /etc/rc.local
--- onestep/faststr.sh
+++ onestep/faststr.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
-cd
-git clone https://github.com/theolink/rtklib.git
-cd rtklib/app/str2str/gcc
-make
-sudo cp str2str /usr/local/bin/str2str
-sudo chmod +x /usr/local/bin/str2str
-sudo mkdir /etc/rtk/
-sudo mkdir /etc/rtk/log/
-sudo mkdir /etc/rtk/cmd/
-sudo chmod -R 777 /etc/rtk
+cd /usr/local/bin/
+wget https://github.com/theolink/tools/raw/master/rtk/str2str
+chmod +x /usr/local/bin/str2str
+mkdir /etc/rtk/
+mkdir /etc/rtk/log/
+mkdir /etc/rtk/cmd/
+chmod -R 777 /etc/rtk
 cd /etc/rtk/cmd/
 wget https://raw.githubusercontent.com/theolink/tools/master/rtk/m8t_5hz_uart_beidou.cmd
 cd /etc/rtk/
 wget https://raw.githubusercontent.com/theolink/tools/master/rtk/strinit.sh
-sudo chmod +x /etc/rtk/strinit.sh
-sudo touch /etc/rtk/log/strinit.log
-sudo chmod 777 /etc/rtk/log/strinit.log
+chmod +x /etc/rtk/strinit.sh
+touch /etc/rtk/log/strinit.log
+chmod 777 /etc/rtk/log/strinit.log
 sed -i '16i\/etc/rtk/strinit.sh\n' /etc/rc.local
